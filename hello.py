1cb55bad0bb58b533b0b28dc9001c527846ae25f
toXCustom <pawel89@gmail.com>
2024-05-15 13:30:01 +0200

    01h 02min 30sec

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
-# Ask user for their name
-name = input("What's your name? ")
+# Ask user for their name, remove whitespace from str and capitalize user's name
+name = input("What's your name? ").strip().title()
 
 # Say hello to user
 print("hello, " + name)
 print("hello,", name)
+print("hello, ", end="")
+print(name)
+print(f"hello, {name}") 
