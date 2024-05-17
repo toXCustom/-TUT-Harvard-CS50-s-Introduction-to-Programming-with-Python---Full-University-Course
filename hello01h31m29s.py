42ce7521c052460d1472bdf6cbf98b058cf05b54
toXCustom <pawel89@gmail.com>
2024-05-17 16:26:44 +0200

    01h 50m 17s

---
+++ hello01h31m29s.py
@@ -0,0 +1,14 @@
+#def hello(to):
+#    print("hello,", to)
+#
+#name = input("What's your name? ")
+#hello(name)
+
+def main():
+    name = input("What's your name? ")
+    hello(name)
+
+def hello(to):
+    print("hello,", to)
+
+main()
