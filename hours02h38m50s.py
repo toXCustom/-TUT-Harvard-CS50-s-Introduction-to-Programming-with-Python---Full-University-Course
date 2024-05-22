43124fe4fc86b1fe0bd2fece718f320b27172280
toXCustom <pawel89@gmail.com>
2024-05-22 13:41:36 +0200

    02h46m29s

---
+++ hours02h38m50s.py
@@ -0,0 +1,9 @@
+name = input("What's you name? ")
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
