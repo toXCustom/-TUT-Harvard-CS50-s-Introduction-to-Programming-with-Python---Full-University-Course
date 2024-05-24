cb1dfe17022a29509b553a04cec4e84ff600ca70
toXCustom <pawel89@gmail.com>
2024-05-24 14:07:25 +0200

    03h13m22s

---
+++ cat02h47m18s.py
@@ -0,0 +1,11 @@
+i = 3
+
+while i != 0:
+    print("meow-i")
+    i -= 1
+
+j = 0
+
+while j < 3:
+    print("meow-j")
+    j += 1
---
+++ cat_for03h03m45s.py
@@ -0,0 +1,4 @@
+for _ in range(3):
+    print("meow")
+
+print ("meow\n" * 3, end="")
